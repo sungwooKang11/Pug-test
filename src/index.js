c576757b969e7d3ad8acbf7f15dfacc352703cfa
sungwookang <sungwookang11@gmail.com>
2022-03-28 22:30:53 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`Welcome to http://localhost:${PORT}`);
+
+}
+
+app.set("view engine", "pug");
+app.set("views", "src/views");
+app.use("/", (req, res) => res.render("home"));
+app.listen(PORT, handleListening);

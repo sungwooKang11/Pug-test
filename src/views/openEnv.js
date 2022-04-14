315aa5a85f5a14d01b94e22f313f3aae3dcccf03
sungwookang <sungwookang11@gmail.com>
2022-04-14 16:23:50 +0900

    편지 개봉, 모달 생성

--- src/views/openEnv.js
+++ src/views/openEnv.js
@@ -1,8 +1,25 @@
 const opener = document.querySelector(".opener");
 const envelope = document.querySelector(".envelope");
+const letter = document.querySelector(".letter");
 
 const openEnv = () => {
     opener.classList.toggle("active");
+    letter.classList.toggle("popup");
+}
+
+const letterPage = document.querySelector(".letterPage");
+const backBtn = document.querySelector(".backBtn");
+const clicker = document.querySelector(".clicker");
+
+const openModal = () => {
+    letterPage.classList.remove("hidden");
+}
+
+const removeModal = () => {
+    letterPage.classList.add("hidden");
 }
 
 envelope.addEventListener("click", openEnv);
+opener.addEventListener("click", openEnv);
+clicker.addEventListener("click", openModal);
+backBtn.addEventListener("click", removeModal);

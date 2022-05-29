adafdc97284078552ae206b772bc8f1f79a3ed7f
sungwookang <sungwookang11@gmail.com>
2022-05-29 22:24:18 +0900

    0005-2 수정

--- src/views/openEnv.js
+++ src/views/openEnv.js
@@ -1,30 +1,13 @@
 const opener = document.querySelector(".opener");
 const envelope = document.querySelector(".envelope");
 const letter = document.querySelector(".letter");
+const warn = document.querySelector(".warn");
 
 const openEnv = () => {  //봉투 열기
     opener.classList.add("active");
     letter.classList.add("popup");
-}
-
-
-const letterPage = document.querySelector(".letterPage");
-const backBtn = document.querySelector(".backBtn");
-const clicker = document.querySelector(".clicker");
-
-const openModal = () => {   //편지 세부창 열기
-    letterPage.classList.remove("hidden");
-}
-
-const removeModal = () => {   //편지 세부창 닫기
-    letterPage.classList.add("hidden");
+    warn.classList.add("float");
 }
 
 envelope.addEventListener("click", openEnv);
 opener.addEventListener("click", openEnv);
-clicker.addEventListener("click", openModal);
-backBtn.addEventListener("click", removeModal);
-
-
-
-

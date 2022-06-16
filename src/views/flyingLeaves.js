a386ea4486e6219935377c8f6a5608489e0c9d3e
sungwookang <sungwookang11@gmail.com>
2022-06-16 09:26:40 +0900

    loading page

---
+++ src/views/flyingLeaves.js
@@ -0,0 +1,18 @@
+const actionFl = document.querySelector(".flowerCon");
+const core = document.querySelector(".core");
+const topL = document.querySelector(".topL");
+const leftL = document.querySelector(".leftL");
+const rightL = document.querySelector(".rightL");
+const bottomL = document.querySelector(".bottomL");
+
+
+
+const handleClick = () => {
+    topL.classList.toggle("active");
+    leftL.classList.toggle("active");
+    rightL.classList.toggle("active");
+    bottomL.classList.toggle("active");
+    core.classList.toggle("disappear")
+}
+
+actionFl.addEventListener("click", handleClick);
